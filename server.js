a8951c67f753fbbd5e2acfa694e041a6ae400c54
Conary36 <cbeckford144@gmail.com>
2020-03-18 16:46:50 -0400

    created logger+imported dependencies

--- server.js
+++ server.js
@@ -1,13 +1,46 @@
 const express = require('express');
-
+const morgan = require('morgan');
+const helmet = require('helmet');
+const userRouter = require('./users/userRouter.js');
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
+//global middleware
+server.use(express.json());//built in middleware, no need to npm install
+server.use(helmet());
+server.use(morgan('dev'));//third-party middleware, needs to be installed
+server.use(logger);
+server.use('/api/users', userRouter);
+server.use('/api/posts', userRouter);
+//server.use(addName);
+
+// server.get('/', addName, (req, res) => {
+//   const nameInsert = (req.name) ? `${req.name}`: '';
+//   res.send(`<h2>Let's write some middleware ${nameInsert}!</h2>`);
+// });
+
+// server.use(notFound);
+
+// function notFound(req, res, next){
+//     res.status(400)
+//     .json({message: "Your Lost, Try again!"});
+// }
+
 
 //custom middleware
+// function addName(req, res, next){
+//   req.name = 'Webpt 11';
+//   next();
+// }
+function logger(req, res, next) {
+  //log information about the request to the console
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url} ${req.get('Origin')}`)
 
-function logger(req, res, next) {}
+  // const method = req.method;
+  // const endpoint = req.originalUrl;
+
+  // console.log(`${method} to ${endpoint}`);
+  next();//moves request to next middleware
+}
+
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,17 +1,72 @@
 const express = require('express');
-
+const coolRouter = require('./userDb')
 const router = express.Router();
 
 router.post('/', (req, res) => {
   // do your magic!
+  const name = req.body;
+  !name ? res.status(400).json({ success: false, errorMessage: "Please provide name!" }) :
+  coolRouter.insert(name)
+            .then(subj =>{
+                console.log('Name added', subj)
+                res.status(201).json(subj);
+            })
+            .catch(error =>{
+              console.log(error);
+              res.status(500).json({
+                errorMessage:
+                " No luck with the post request!"
+              });
+            })
 });
 
 router.post('/:id/posts', (req, res) => {
   // do your magic!
+  const {user_id, text} = req.body;
+  !user_id ? res.status(404).json({ success: false, errorMessage: "The post with the specified ID does not exist." }) :
+  coolRouter.insert({user_id,text})
+            .then(data =>{
+              if(data){
+                  res.status(201).json({user_id, text})
+              }else{
+                null
+              }
+            })
+            .catch(err =>{
+              if(!text){
+                  res.status(400).json({
+                    success: false, errorMessage: "Please provide text for the post.",err
+                  })
+              }else{
+                console.log(error);
+                res 
+                    .status(500)
+                    .json({
+                      success: false, 
+                      errorMessage:
+                        "There was an error while saving the post"
+                 })
+              }
+            })
+
+
 });
 
 router.get('/', (req, res) => {
   // do your magic!
+  coolRouter.get(req.query)
+            .then(data =>{
+              res.status(200).json(data);
+            })
+            .catch(err =>{
+              res
+                .status(500)
+                .json({
+                  success: false,
+                  errorMessage:
+                    "The posts information could not be retrieved.", err
+                })
+            })
 });
 
 router.get('/:id', (req, res) => {
@@ -34,6 +89,15 @@ router.put('/:id', (req, res) => {
 
 function validateUserId(req, res, next) {
   // do your magic!
+  const {id} = req.params;
+  coolRouter.getById(id)
+         .then()
+        if(id){
+
+        }else{
+
+        }
+
 }
 
 function validateUser(req, res, next) {
